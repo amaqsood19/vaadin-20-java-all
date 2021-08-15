96caf4983f55c5f4d6ea6a8483787af4a8cdacba
start.vaadin.com <no-reply@vaadin.com>
2021-08-15 12:28:42 +0000

    Generated project

---
+++ src/main/java/com/luxoft/mx/tool/ui/views/empty/EmptyView.java
@@ -0,0 +1,20 @@
+package com.luxoft.mx.tool.ui.views.empty;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.PageTitle;
+import com.luxoft.mx.tool.ui.views.MainLayout;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Empty")
+@Route(value = "empty", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class EmptyView extends Div {
+
+    public EmptyView() {
+        addClassName("empty-view");
+        add(new Text("Content placeholder"));
+    }
+
+}
